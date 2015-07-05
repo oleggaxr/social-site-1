12482b1893390efb4262bb860d9059366cf3bcd8
artur <artur@mail.ru>
2015-07-05 19:57:41 +0300

    mee

--- application/views/header.php
+++ application/views/header.php
@@ -26,13 +26,13 @@
             <tr/><td><img src="/images/logo.png" class="logo" height="180" width="180"></td>
                 <td class="text"><p>При отделе Социального служения Казанской Епархии</p></td>
 				<td>
-						<div style = "margin-left:200px;"><h3><p style = "color:#6CA6CD;font-size:25px;"><? echo form_prep($this->session->userdata('prop_login')); ?></p></h3></div>
-						<div style = "margin-left:200px;"><?php if($this->session->userdata('prop_login') == TRUE){?><a style = "text-decoration: none;color:black;color:#6CA6CD;" href = "<?php echo site_url("admin/admin_news");?>">Меню администратора</a><?}?></div>
-						<div style = "margin-left:200px;"><?php if($this->session->userdata('prop_login') == TRUE){?><a class = "destroy" style = "text-decoration: none;color:white;" href = "<?php echo site_url("main/logout");?>">Выйти</a><?}?></div>
+						<div style = "margin-left:200px;"><h3><p style = "color:#6CA6CD;font-size:25px;"><?php echo form_prep($this->session->userdata('prop_login')); ?></p></h3></div>
+						<div style = "margin-left:200px;"><?php if($this->session->userdata('prop_login') == TRUE){?><a style = "text-decoration: none;color:black;color:#6CA6CD;" href = "<?php echo site_url("admin/admin_news");?>">Меню администратора</a><?php } ?></div>
+						<div style = "margin-left:200px;"><?php if($this->session->userdata('prop_login') == TRUE){?><a class = "destroy" style = "text-decoration: none;color:white;" href = "<?php echo site_url("main/logout");?>">Выйти</a><?php } ?></div>
 				</td>
             </table>
 		
-			<div style = "margin-left:480px;"><?php if($this->session->userdata('prop_login') == FALSE){?><p><a href="#openModal" class = "downloadavatar" style = "color:#6CA6CD;">ВходДляАдминистратора</a></p><?}?></div>
+			<div style = "margin-left:480px;"><?php if($this->session->userdata('prop_login') == FALSE){?><p><a href="#openModal" class = "downloadavatar" style = "color:#6CA6CD;">ВходДляАдминистратора</a></p><?php } ?></div>
 			
 			<div id="openModal" class="modalDialog">
 						<div>
