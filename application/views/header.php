84406112c2d7bf8c07167b622d9c8cc4ecf18010
Ridman <controlservice@yandex.ru>
2015-07-05 18:52:51 +0300

    Вернул бутстрап

--- application/views/header.php
+++ application/views/header.php
@@ -7,6 +7,9 @@
     <!-- Bootstrap next 5 lines -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 
     <title>
         <?= $title ?>
@@ -20,78 +23,78 @@
                 <td class="text"><p>При отделе Социального служения Казанской Епархии</p></td>
             </table>
         </div>
-            <ul class="nav nav-pills" id="nav">
+            <ul id="nav">
                 <li class="<?= $controller_name === "main" ? "active" : ""?>"><a href="/">Главная</a></li>
                 <li class="<?= $controller_name === "about" ? "active" : ""?> dropdown">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                    <a href="#">
                         О нас
-                        <b class="caret"></b>
+                        
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul>
                         <li><a href="/about/department">Отдел</a></li>
                         <li><a href="/about/worship">Служба</a></li>
                         <li><a href="/about/documents">Документы</a></li>
                     </ul>
                 </li>
-                <li class="<?= $controller_name === "projects" ? "active" : ""?> dropdown pull-left">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                <li class="<?= $controller_name === "projects" ? "active" : ""?>">
+                    <a href="#">
                         Проекты
-                        <b class="caret"></b>
+                        
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul>
                         <li><a href="/projects/cpm">Центр поддержки материнства</a></li>
                         <li><a href="/projects/volunteers">Добровольцы</a></li>
                         <li><a href="/projects/sisterhood">Сестричество</a></li>
                         <li><a href="/projects/soberness">Трезвость</a></li>
                     </ul>
                 </li>
-                <li class="<?= $controller_name === "reports" ? "active" : ""?> dropdown pull-left">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                <li class="<?= $controller_name === "reports" ? "active" : ""?>">
+                    <a href="#">
                         Отчёты
-                        <b class="caret"></b>
+                        
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul>
                         <li><a href="/reports/financial">Финансовые отчёты</a></li>
                         <li><a href="/reports/helpto">Кому помогли</a></li>
                         <li><a href="/reports/helpfrom">Кто помог</a></li>
                     </ul>
                 </li>
-                <li class="<?= $controller_name === "benefactor" ? "active" : ""?> dropdown pull-left">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                <li class="<?= $controller_name === "benefactor" ? "active" : ""?>">
+                    <a href="#">
                         Помощь
-                        <b class="caret"></b>
+                        
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul>
                         <li><a href="/benefactor/to">Пожертвовать</a></li>
                         <li><a href="/benefactor/from">Попросить помощи</a></li>
                         <li><a href="/benefactor/be">Стать добровольцем</a></li>
                         <li><a href="/benefactor/pray">Помолиться</a></li>
                     </ul>
                 </li>
-                <li class="<?= $controller_name === "help" ? "active" : ""?> dropdown pull-left">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                <li class="<?= $controller_name === "help" ? "active" : ""?>">
+                    <a href="#">
                         Полезное
-                        <b class="caret"></b>
+                        
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul>
                         <li><a href="/help/where">Куда обратиться за помощью</a></li>
                         <li><a href="/help/todo">Что делать в трудной ситуации</a></li>
                         <li><a href="/help/books">Полезные книги</a></li>
                     </ul>
                 </li>
-                <li class="<?= $controller_name === "media" ? "active" : ""?> dropdown pull-left">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                <li class="<?= $controller_name === "media" ? "active" : ""?>">
+                    <a href="#">
                         Медиа
-                        <b class="caret"></b>
+                        
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul>
                         <li><a href="/media/about">СМИ о нас</a></li>
                         <li><a href="/media/photo">Фото</a></li>
                         <li><a href="/media/video">Видео</a></li>
                         <li><a href="/media/results">Работы подопечных</a></li>
                     </ul>
                 </li>
-                <li class="<?= $controller_name === "contact" ? "active" : ""?> pull-left">
+                <li class="<?= $controller_name === "contact" ? "active" : ""?>">
                     <a href="/main/contact">Контакты</a>
                 </li>
             </ul>
