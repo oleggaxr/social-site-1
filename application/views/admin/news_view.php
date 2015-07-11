caba1ca676e42d7410c08e1667836ee15853a420
Ridman <controlservice@yandex.ru>
2015-07-11 22:35:08 +0300

    Админка верстка

--- application/views/admin/news_view.php
+++ application/views/admin/news_view.php
@@ -1,37 +1,44 @@
 <div id="panel">
+
     <?php echo form_open_multipart('admin/addnews'); ?>
     <table>
         <tr>
             <td>
-                <p>Название статьи</p>
-                <textarea name="namenews"></textarea>
+                <h3>Название статьи</h3>
+            </td>
+            <td>
+                <textarea name="namenews" cols="100"></textarea>
             </td>
         </tr>
         <tr>
             <td>
-                <p>Содержание статьи</p>
-                <textarea name="textnews"></textarea>
+                <h3>Содержание статьи</h3>
+            </td>
+            <td>
+                <textarea name="textnews" cols="100" rows="10"></textarea>
             </td>
         </tr>
         <tr>
             <td>
                 <!--<p style = "margin-top:10px;"><a href="#openModal" class = "downloadavatar" style = "color:#6CA6CD;">Загрузить картинку</a></p>-->
-                <input type="file" name="upload">
+
 
             </td>
         </tr>
         <tr>
             <td>
-                <p>Выберите категорию статьи</p>
-                <input type="checkbox" name="shares" value="Акции">Акции
-                <input type="checkbox" name="directions" value="a2">Направления
-                <input type="checkbox" name="who_help" value="a2">Кому помогли
+                <h3>Выберите категорию статьи</h3>
+                <ul list-style-type="none">
+                    <li/><input type="checkbox" name="shares" value="Акции">Акции
+                    <li/><input type="checkbox" name="directions" value="a2">Направления
+                    <li/><input type="checkbox" name="who_help" value="a2">Кому помогли
+                </ul>
             </td>
         </tr>
         <tr>
             <td>
                 <p>Загрузить картинку</p>
-
+                <input type="file" name="upload">
 
             </td>
         </tr>
@@ -41,29 +48,26 @@
     <?php echo form_close(); ?>
 
 </div>
-<div style="margin-top:50px;">
-    <table>
-        <tr>
-            <td>
-                <?php
-                //$this->db->select(('prop_text', 1, 30), 'prop_namenews','prop');
-                $query = $this->db->get('prop_news');
-                foreach ($query->result() as $row)
-                {
-                echo "<font style = ';font:18px Arial, Helvetica, sans-serif;'>" . $row->prop_namenews . "</font><br><br>";
-                echo "<img style = 'width:600px;height:400px;' src = '" . base_url() . 'images/' . $row->prop_images . "'><br><br>";
-
-                ?>
+<?php
+//$this->db->select(('prop_text', 1, 30), 'prop_namenews','prop');
+$query = $this->db->get('prop_news');
+foreach ($query->result() as $row) {
+    ?>
+    <div class="news">
+        <table>
+            <tr/>
+            <td width="15%"><img height="100" width="140" src="<?= base_url() . 'images/' . $row->prop_images ?>"></td>
+            <td width="80%">
+                <a href="#"><h3><?= $row->prop_namenews ?></h3></a>
+                <span class="date">10.10.2015</span>
+                <textarea cols="100" rows="15"><?= $row->prop_text ?></textarea>
             </td>
-        </tr>
-        <tr>
-            <td>
-                <?php
-
-                echo $row->prop_text . "<br><br>";
-                }
-                ?>
+            <td width="5%">
+                <input type="button" value="Удалить">
+                <input type="button" value="Изменть">
             </td>
-        </tr>
-    </table>
-</div>
+        </table>
+    </div>
+<?php
+}
+?>
